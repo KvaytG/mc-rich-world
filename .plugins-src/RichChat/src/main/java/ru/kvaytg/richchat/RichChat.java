bc0cb22fefc05846c193556abc595fbfa907baf1
KvaytG <vkon2019@inbox.ru>
2026-09-07 20:22:28 +0300

    Updated RichChat plugin

--- .plugins-src/RichChat/src/main/java/ru/kvaytg/richchat/RichChat.java
+++ .plugins-src/RichChat/src/main/java/ru/kvaytg/richchat/RichChat.java
@@ -1,13 +1,18 @@
 package ru.kvaytg.richchat;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.kvaytg.richdonate.paper.RichDonate;
 
 @SuppressWarnings("unused")
 public class RichChat extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new ChatHandler(), this);
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(
+                new ChatHandler(this, RichDonate.getInstance()),
+                this
+        );
     }
 
 }
